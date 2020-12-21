c6cc44f83d0285dc96a56fd028a03c1a91a6ffae
KC5-BP <kevin.bougnon@heig-vd.ch>
2020-12-21 18:05:49 +0100

    Matrix : Files renamed, plus commenting linker to files' user. Those files must be copied into the project's folder instead of using it as a general purpose.

--- Matrix/ws2812_matrix.h
+++ Matrix/ws2812_matrix.h
@@ -1,18 +1,19 @@
-/*-->	Title	:	KBP_NeoPixel
+/*-->	Title	:	ws2812_matrix
 		  |
 		  '->	Source	:	Timers_Wait_Xms.h (One of my own first header file)
 		  |
 		  '->	Creation	:	21.11.2019
 		  :			|
-		  :			'->	Last Update	:	26.11.2019
+		  :			'->	Last Update	:	26.11.2020
 		  |
 		  '->	Description	:	Give the ability to use NeoPixels func. from
 								an external 'C' file.
 */
 /*======================================================================================>
 =======================================================================================*/
-// Linker to : ...
-#include "base_sfr.h"	// ... Base (SFR, sbit, define, var. type, etc...)
+// Linker to : ..
+// .. Base (SFR, sbit, define, var. type, etc...)
+//#include "../F12-NeoPix_MatrixPrButt/base_sfr.h"	
 
 // Library Definition :
 // Beginning of definition...
@@ -21,21 +22,21 @@
 
 //===================================================
 //================================\Header's_Definitions/================================>
-
-// In case of Matrix :
-#define MAX_LEDS 256 // Number of LED used with Matrix
+#define MAX_LEDS 256 // Number of LEDs used with Matrix
 #define MATRIX_NBR 1
-// Type of Matrix...
+// Type of Matrix ...
 #define MAX_LINE 16
 #define MAX_COLUMN 16
 
 // Basic Color Intensities :
 #define BRIGHT_MAX 0xFF	// Value to make light a LED (MAX Intensity).
 #define BRIGHT_MIN 0x00	// Value to make light a LED (MIN Intensity).
+
 #define BRIGHT_MID 0x36	// Value to make light a LED (MID Intensity).
 #ifndef BRIGHT_MID
 #define BRIGHT_MID 0x10	// Value to make light a LED (MID Intensity).
 #endif
+
 #define STEP_COLOR	0x05	// Value to In-/De- crease the Intensity of a Color.
 
 // In case of Text Printing : Char. Aire - Width
@@ -49,37 +50,37 @@
 #define OFF_CHAR_DIM_7X6	35
 
 //-- GLOBAL TYPE		:------------------------------->
-// Description : Creation of color type based R-G-B
+// Description : Creation of color type based R-G-B ..
 typedef struct {
 	unsigned char Red;
-   unsigned char Green;
-   unsigned char Blue;
-}color; //... named color.
+  unsigned char Green;
+  unsigned char Blue;
+}color; // .. named color.
 
-// Description : Creation of the definition of one LED (WS281x).
+// Description : Creation of the definition of one LED (WS281x) ..
 typedef struct {
 	color colorPix;	      // color (Red, Green, Blue),
 	unsigned char status;   // status (ON : 1 / OFF : 0),
 	unsigned int pos;       // And a Position.
-}pixel; //... named pixel.
+}pixel; // .. named pixel.
 
-// Description : Creation of the offset position for Text Printing.
+// Description : Creation of the offset position for Text Printing ..
 typedef struct {
    unsigned char column;   // X val. for Column.
    unsigned char line;     // Y val. for Line.
-}offsetText; //... named offsetText.
+}offsetText; // .. named offsetText.
 
-// Description : Matrix format
+// Description : Matrix format ..
 typedef struct {
    unsigned int matrixView;   // Matrix orientation : 0-90-... degrees
    unsigned char txtFont;     // Font used like, Classic, Time Number or Mob style.
-}matrixFormat; //... named matrixFormat.
+}matrixFormat; // .. named matrixFormat.
 
 enum enum_Color
 {	Null = 0,
 	Red,
 	Green,
-   Blue,
+	Blue,
 	RedGreen,
 	RedBlue,
 	GreenBlue,
@@ -143,7 +144,8 @@ extern xdata matrixFormat matrixDisplay;
 						_nop_();\
 						_nop_();\
 					}
-// .. for sending bit to bit for each color in through iteration.
+					
+// .. sending ONE bit of A specific color.
 #define	SEND_TO_LED(COLOR, MASK) { if((COLOR & MASK) != 0) {SEND1();} else{SEND0();} }
 
 //-------------------------------------------------------------------------------------->
@@ -160,31 +162,32 @@ extern unsigned int pixel_PosRecovery(unsigned char posX, unsigned char posY);
  * Input		   :	addressMatrix, 6bytes - address of the matrix
  *						newColor, 3bytes - color to set
  *						position, 2bytes - position in the matrix to set the color.
- * Output	 	:	Nothin'                                                              */
-extern void pixel_SetColor(pixel* addressMatrix, color newColor, unsigned int position);
+ * Output	 	:	Nothin'                                                                  */
+extern void pixel_SetColor(pixel* addressMatrix, color newColor, \
+													 unsigned int position);
 /* Description :	Send the 24bits color of one pixel.
  * Last_Update :  21.12.2020
  * Input			:	red, 1byte.
  *						green, 1byte.
  *						blue, 1byte.
- * Output	 	:	Nothin'                                                              */
+ * Output	 	:	Nothin'                                                                  */
 void pixel_Show(unsigned char red, unsigned char green, unsigned char blue);
 
 
 /* Description :	Clear color & status on all the matrix + display it (or shut it).
  * Last_Update :  21.12.2020
  * Input		   :	addressMatrix, 6bytes - address of the matrix
- * Output	 	:	Nothin'                                                              */
+ * Output	 	:	Nothin'                                                                  */
 extern void matrix_Clear(pixel* addressMatrix);
 /* Description :	Clear ONLY status on all the matrix, to keep the color if needed.
  * Last_Update :  21.12.2020
  * Input		   :	addressMatrix, 6bytes - address of the matrix
- * Output	 	:	Nothin'                                                              */
+ * Output	 	:	Nothin'                                                                  */
 extern void matrix_StatusReset(pixel* addressMatrix);
 /* Description :	Show the entire matrix through output pin.
  * Last_Update :  21.12.2020
  * Input		   :	addressMatrix, 6bytes - address of the matrix
- * Output	 	:	Nothin'                                                              */
+ * Output	 	:	Nothin'                                                                  */
 extern void matrix_Show(pixel* addressMatrix);
 
 
@@ -192,23 +195,25 @@ extern void matrix_Show(pixel* addressMatrix);
  * Last_Update :  21.12.2020
  * Input       :  posX,	1byte - Column (from 1 instead of 0)
  *             :  posY,	1byte - Line (from 1 instead of 0)
- * Output	 	:	Nothin'                                                              */
+ * Output	 	:	Nothin'                                                                  */
 extern void matrix_SetTextPos(unsigned char  column, unsigned char line);
 /* Description :	Printing text on Matrix.
  * Last_Update :  21.12.2020
  * Input		   :	addressMatrix, 6bytes - address of the matrix
  *						newColor, 3bytes - color to set
  *						position, 2bytes - position in the matrix to set the color.
- * Output	 	:	Nothin'                                                              */
-extern void matrix_Print(pixel* addressMatrix, color newColor, const char* _CharToWrite, ...);
+ * Output	 	:	Nothin'                                                                  */
+extern void matrix_Print(pixel* addressMatrix, color newColor, \
+												 const char* _CharToWrite, ...);
 
 /* Description :	Setting a group of consecutive LED to a color.
  * Last_Update :  21.12.2020
  * Input		   :	addressMatrix, 6bytes - address of the matrix
  *						newColor, 3bytes - color to set
  *						position, 2bytes - position in the matrix to set the color.
- * Output	 	:	Nothin'                                                              */
-extern void strip_LedChain(pixel* addressMatrix, color newColor, unsigned int start, unsigned int end);
+ * Output	 	:	Nothin'                                                                  */
+extern void strip_LedChain(pixel* addressMatrix, color newColor, \
+													 unsigned int start, unsigned int end);
 
 
 #endif
