df97f0b4dfaa0d1905f257248dcad28c58190126
KC5-BP <kevin.bougnon@heig-vd.ch>
2020-12-25 01:06:07 +0100

    Strip : Cleaning code indentation + reorder function's list ..

--- Strip/ws2812_strip.c
+++ Strip/ws2812_strip.c
@@ -1,155 +1,185 @@
-/*-->	Title	:	KBP_NeoPixel_SM
+/*-->	Title	:	ws2812_strip
 		  |
 		  '->	Source	:	Timers_Wait_Xms.h (One of my own first header file)
 		  |
 		  '->	Creation	:	21.11.2019
 		  :			|
-		  :			'->	Last Update	:	March 2020
+		  :			'->	Last Update	:	December 2020
 		  |
 		  '->	Description	:	Declarations of NeoPixels Usage functions.
 */
-/*========================================================================================>
-=========================================================================================*/
-// Linker to : ...
-#include <c8051f020.h>		// ... definition folder SFR 
-													// (Like Port Definition "P5", "P6", etc...)
-// ... C51/C166 routines that instructs the compiler to generate intrinsic code | _nop_():
+/*======================================================================================>
+=======================================================================================*/
+// Linker to : ..
+// .. definition folder SFR (Like Port Definition "P5", "P6", etc...)
+#include <c8051f020.h>
+// .. C51/C166 routines that instructs the compiler to generate intrinsic code | _nop_():
 #include <intrins.h>
-#include "../F11-NeoPix_StripPrButt/base_sfr.h"	// ... Base (SFR, sbit, define, var. type, etc...)
-//#include "../F11-NeoPix_StripPrButt/time.h"		// ... Timers function and Waiting Function.
-#include "ws2812_strip.h"	// ... Timers function and Waiting Function.
+// .. Base (SFR, sbit, define, var. type, etc...)
+#include "../F11-NeoPix_StripPrButt/base_sfr.h"
+// .. Timers function and Waiting Function.
+//#include "../F11-NeoPix_StripPrButt/time.h"
+// .. created header for NeoPixel management.
+#include "ws2812_strip.h"
 
-//-- GLOBAL VARIABLES	INIT:------------------------------->
-xdata pixel strip[MAX_LEDS] = {{0, 0, 0}, 0, 0};   // Initialize it to '0'.
-// & the Position is a Var. that can be unused.
+//-- GLOBAL VARIABLES	INIT:----------------------------->
+xdata pixel strip[MAX_LEDS] = {{0, 0, 0}, 0};    // Initialize Strip to '0'.
 
 void pixel_SetColor(pixel* addressStrip, color newColor, unsigned int position)
-{	// Var. Dec. :
-   // To avoid problems about the parameters, I preferred to work with copies.
-   //unsigned char Red = newColor.Red;
-   //unsigned char Green = newColor.Green;
-   //unsigned char Blue = newColor.Blue;
+{   // Go to the wanted LED position.
+    addressStrip += (position - 1);
 
-   addressStrip += (position - 1);	// go to the wanted LED position.
+    // Set "color" values :
+    addressStrip->colorPix = newColor;
 
-   // Set "color" values :
-   //addressStrip->colorPix.Red = Red;
-   //addressStrip->colorPix.Green = Green;
-   //addressStrip->colorPix.Blue = Blue;
-   addressStrip->colorPix = newColor;
-	// Could write addressStrip[position - 1]. ... but it takes more code operations.
-
-   // Set the position info. in the structure :
-   addressStrip->pos = position;
-
-   // Set the status to ON if color different of "black" :
-	 if((newColor.Red == 0) && (newColor.Green == 0) && (newColor.Blue == 0))
-	 {	// Complement : == operator not possible in C.
-			addressStrip->status = 0;
-	 }
-	 else
-	 {
-			addressStrip->status = 1;
-	 }
+    // Set the status to ON if color different of "black" :
+    if((newColor.Red == 0) && (newColor.Green == 0) && (newColor.Blue == 0))
+    {   // Complement : " == " operator not possible in C.
+        addressStrip->status = 0;
+    }
+    else
+    {
+        addressStrip->status = 1;
+    }
 }
 
 void pixel_Show(unsigned char red, unsigned char green, unsigned char blue)
-{	// Var. Dec. :
-   // To avoid problems about the parameters, I preferred to work with copies.
-   unsigned char tmpRed = red;
-   unsigned char tmpGreen = green;
-   unsigned char tmpBlue = blue;
-
-   // For the WS2812b, the order is High bit to low AND Green - Red - Blue.
-   // Sending GREEN.
-   SEND_TO_LED(tmpGreen, BIT7);
-   SEND_TO_LED(tmpGreen, BIT6);
-   SEND_TO_LED(tmpGreen, BIT5);
-   SEND_TO_LED(tmpGreen, BIT4);
-   SEND_TO_LED(tmpGreen, BIT3);
-   SEND_TO_LED(tmpGreen, BIT2);
-   SEND_TO_LED(tmpGreen, BIT1);
-   SEND_TO_LED(tmpGreen, BIT0);
-   // Sending RED.
-   SEND_TO_LED(tmpRed, BIT7);
-   SEND_TO_LED(tmpRed, BIT6);
-   SEND_TO_LED(tmpRed, BIT5);
-   SEND_TO_LED(tmpRed, BIT4);
-   SEND_TO_LED(tmpRed, BIT3);
-   SEND_TO_LED(tmpRed, BIT2);
-   SEND_TO_LED(tmpRed, BIT1);
-   SEND_TO_LED(tmpRed, BIT0);
-   // Sending BLUE.
-   SEND_TO_LED(tmpBlue, BIT7);
-   SEND_TO_LED(tmpBlue, BIT6);
-   SEND_TO_LED(tmpBlue, BIT5);
-   SEND_TO_LED(tmpBlue, BIT4);
-   SEND_TO_LED(tmpBlue, BIT3);
-   SEND_TO_LED(tmpBlue, BIT2);
-   SEND_TO_LED(tmpBlue, BIT1);
-   SEND_TO_LED(tmpBlue, BIT0);
+{   // For the WS2812b, the order is High bit to low AND Green - Red - Blue.
+    // Sending GREEN.
+    SEND_TO_LED(green, BIT7);
+    SEND_TO_LED(green, BIT6);
+    SEND_TO_LED(green, BIT5);
+    SEND_TO_LED(green, BIT4);
+    SEND_TO_LED(green, BIT3);
+    SEND_TO_LED(green, BIT2);
+    SEND_TO_LED(green, BIT1);
+    SEND_TO_LED(green, BIT0);
+    // Sending RED.
+    SEND_TO_LED(red, BIT7);
+    SEND_TO_LED(red, BIT6);
+    SEND_TO_LED(red, BIT5);
+    SEND_TO_LED(red, BIT4);
+    SEND_TO_LED(red, BIT3);
+    SEND_TO_LED(red, BIT2);
+    SEND_TO_LED(red, BIT1);
+    SEND_TO_LED(red, BIT0);
+    // Sending BLUE.
+    SEND_TO_LED(blue, BIT7);
+    SEND_TO_LED(blue, BIT6);
+    SEND_TO_LED(blue, BIT5);
+    SEND_TO_LED(blue, BIT4);
+    SEND_TO_LED(blue, BIT3);
+    SEND_TO_LED(blue, BIT2);
+    SEND_TO_LED(blue, BIT1);
+    SEND_TO_LED(blue, BIT0);
 }
 
+//-------------------------------------------------------------------------------------->
+void strip_Show(pixel* addressStrip)
+{   // Var. Dec. :
+    xdata uint16 i;
+
+    // Disable Timer to avoid interrupting Sending Paquets :
+    TR0 = 0;
+
+    for(i = 0; i < MAX_LEDS; i++)
+    {
+        if(addressStrip->status != 0)
+        {
+            pixel_Show(addressStrip->colorPix.Red, addressStrip->colorPix.Green, \
+                        addressStrip->colorPix.Blue);
+        }
+        else
+        {
+            pixel_Show(BRIGHT_MIN, BRIGHT_MIN, BRIGHT_MIN);
+        }
+        addressStrip++;
+    }
+
+    // Enable / Re-activate Timer after Packets Sent :
+    TR0 = 1;
+}
 
 void strip_Clear(pixel* addressStrip)
-{	// Var. Dec. :
-   unsigned int i;
-   pixel* addressSave = addressStrip;
+{   // Var. Dec. :
+    unsigned int i;
+    pixel* addressSave = addressStrip;
 
-   for(i = 0; i < MAX_LEDS; i++)
-   {	// Clear ALL Color in the original addresses ...
-      addressStrip->colorPix.Red = BRIGHT_MIN;
-      addressStrip->colorPix.Green = BRIGHT_MIN;
-      addressStrip->colorPix.Blue = BRIGHT_MIN;
+    for(i = 0; i < MAX_LEDS; i++)
+    {	// Clear ALL Color at the original address ...
+        addressStrip->colorPix.Red = BRIGHT_MIN;
+        addressStrip->colorPix.Green = BRIGHT_MIN;
+        addressStrip->colorPix.Blue = BRIGHT_MIN;
 
-      addressStrip->pos = 0; // Clear position ..
+        addressStrip->status = 0; // Clear status ..
 
-      addressStrip->status = 0; // Clear status ..
+        addressStrip++;  // Increase address for clearing next position.
+    }
 
-      addressStrip++;  // Increase address for clearing next position.
-   }
-   strip_Show(addressSave);
+    strip_Show(addressSave);
 }
 
-void strip_StatusReset(pixel* addressMatrix)
-{	// Var. Dec. :
-   unsigned int i;
+void strip_StatusReset(pixel* addressStrip)
+{   // Var. Dec. :
+    unsigned int i;
 
-   for(i = 0; i < MAX_LEDS; i++)
-   {	// Clear Status to keep color in the original addresses ...
-      addressMatrix->status = 0;
+    for(i = 0; i < MAX_LEDS; i++)
+    {   // Clear Status to keep color in the original address ...
+        addressStrip->status = 0;
 
-      addressMatrix++;  // Increase address for clearing next position.
-   }
+        addressStrip++;  // Increase address for clearing next position.
+    }
 }
 
-void strip_Show(pixel* addressStrip)
+void strip_StatusInverter(pixel* addressStrip)
 {	// Var. Dec. :
-   xdata uint16 i;
+    /*xdata uint16 i;	// LED Position for filling Board of neoPix.
+    //xdata uint16 j;	// Address recovering counter of the matrix.
+    // To avoid problems about the parameters, I prefered to work with copies.
+    //pixel* AddSave = addressStrip;
+    xdata color tmp = {0, 0, 0};
 
-   // Disable Timer to avoid interrupting Sending Paquets :
-   TR0 = 0;
+    for(i = 0; i < MAX_LEDS; i++)
+    {
+        //if((addressStrip->colorPix.Red != 0) || (addressStrip->colorPix.Green != 0) ||\
+             (addressStrip->colorPix.Blue != 0))
+        if(addressStrip->status != 0)
+        {
+            tmp.Red = addressStrip->colorPix.Red;
+            tmp.Green = addressStrip->colorPix.Green;
+            tmp.Blue = addressStrip->colorPix.Blue;
+            break;
+        }
+        addressStrip++;
+    }
 
-   for(i = 0; i < MAX_LEDS; i++)
-   {
-		 if(addressStrip->status != 0)
-		 {
-			 pixel_Show(addressStrip->colorPix.Red, addressStrip->colorPix.Green, addressStrip->colorPix.Blue);
-		 }
-		 else
-		 {
-			 pixel_Show(BRIGHT_MIN, BRIGHT_MIN, BRIGHT_MIN);
-		 }
-		 addressStrip++;
-   }
+// 	for(i = 0; i < j; i++)
+// 	{
+// 		addressStrip--;
+// 	}
 
-   // Enable / Re-activate Timer after Paquets Sent :
-   TR0 = 1;
+    for(i = 0; i < MAX_LEDS; i++)
+    {
+        if(addressStrip[i].status == 0)
+        {
+            addressStrip[i].status = 1;
+            addressStrip[i].colorPix.Red = tmp.Red;
+            addressStrip[i].colorPix.Green = tmp.Green;
+            addressStrip[i].colorPix.Blue = tmp.Blue;
+        }
+        else
+        {
+            addressStrip[i].status = 0;
+            addressStrip[i].colorPix.Red = BRIGHT_MIN;
+            addressStrip[i].colorPix.Green = BRIGHT_MIN;
+            addressStrip[i].colorPix.Blue = BRIGHT_MIN;
+        }
+        //addressStrip++;
+    }*/
 }
-
 
 void strip_LimitPos(pixel* addressStrip, color newColor, \
-													 unsigned int _Begin, unsigned int _End)
+											    unsigned int _Begin, unsigned int _End)
 {	// Var. Dec. :
 	uint16 i;	// LED Position for filling Board of neoPix.
 	// To avoid problems about the parameters, I prefered to work with copies.
@@ -174,48 +204,3 @@ void strip_LimitPos(pixel* addressStrip, color newColor, \
 	strip_Show(AddSave);
 }
 
-void strip_StatusInverter(pixel* addressStrip)
-{	// Var. Dec. :
-	/*xdata uint16 i;	// LED Position for filling Board of neoPix.
-	//xdata uint16 j;	// Address recovering counter of the matrix.
-	// To avoid problems about the parameters, I prefered to work with copies.
-	//pixel* AddSave = addressStrip;
-	xdata color tmp = {0, 0, 0};
-
-	for(i = 0; i < MAX_LEDS; i++)
-	{
-		//if((addressStrip->colorPix.Red != 0) || (addressStrip->colorPix.Green != 0) || (addressStrip->colorPix.Blue != 0))
-		if(addressStrip->status != 0)
-		{
-			tmp.Red = addressStrip->colorPix.Red;
-			tmp.Green = addressStrip->colorPix.Green;
-			tmp.Blue = addressStrip->colorPix.Blue;
-			break;
-		}
-		addressStrip++;
-	}
-
-// 	for(i = 0; i < j; i++)
-// 	{
-// 		addressStrip--;
-// 	}
-	
-	for(i = 0; i < MAX_LEDS; i++)
-	{
-		if(addressStrip[i].status == 0)
-		{
-			addressStrip[i].status = 1;
-			addressStrip[i].colorPix.Red = tmp.Red;
-			addressStrip[i].colorPix.Green = tmp.Green;
-			addressStrip[i].colorPix.Blue = tmp.Blue;
-		}
-		else
-		{
-			addressStrip[i].status = 0;
-			addressStrip[i].colorPix.Red = BRIGHT_MIN;
-			addressStrip[i].colorPix.Green = BRIGHT_MIN;
-			addressStrip[i].colorPix.Blue = BRIGHT_MIN;
-		}
-		//addressStrip++;
-	}*/
-}
--- Strip/ws2812_strip.h
+++ Strip/ws2812_strip.h
@@ -1,152 +1,155 @@
-/*-->	Title	:	KBP_NeoPixel
+/*-->	Title	:	ws2812_strip
 		  |
 		  '->	Source	:	Timers_Wait_Xms.h (One of my own first header file)
 		  |
 		  '->	Creation	:	21.11.2019
 		  :			|
-		  :			'->	Last Update	:	26.11.2019
+		  :			'->	Last Update	:	24.12.2020
 		  |
 		  '->	Description	:	Give the ability to use NeoPixels func. from
 								an external 'C' file.
 */
-/*========================================================================================>
-=========================================================================================*/
+/*======================================================================================>
+=======================================================================================*/
 // Linker to (if needed) : ..
 
 
-// Library Definition :
+// Header's Definition :
 // Beginning of definition...
-#ifndef  __ws281x_strip__
-#define  __ws281x_strip__
+#ifndef __ws2812_strip__
+#define __ws2812_strip__
+
 
 //===================================================
-//================================\Header's_Definitions/==================================>
+//================================\Header's_Definitions/================================>
 #define MAX_LEDS 300 // Number of LED used.
 
 // Basic Color Intensities :
-#define BRIGHT_MAX 0xFF	// Value to make light a LED (MAX Intensity).
-#define BRIGHT_MIN 0x00	// Value to make light a LED (MIN Intensity).
+#define BRIGHT_MAX 0xFF // Value to make light a LED (MAX Intensity).
+#define BRIGHT_MIN 0x00 // Value to make light a LED (MIN Intensity).
 
-#define BRIGHT_MID 0x36	// Value to make light a LED (MID Intensity).
+#define BRIGHT_MID 0x36 // Value to make light a LED (MID Intensity).
 #ifndef BRIGHT_MID
-#define BRIGHT_MID 0x10	// Value to make light a LED (MID Intensity).
+#define BRIGHT_MID 0x10 // Value to make light a LED (MID Intensity).
 #endif
 
-#define STEP_COLOR	0x05	// Value to In-/De- crease the Intensity of a Color.
+#define STEP_COLOR 0x05 // Value to In-/De- crease the Intensity of a Color.
 
-//-- GLOBAL TYPE		:------------------------------->
+//-- GLOBAL TYPE		:--------------------------------->
 // Description : Creation of color type based R-G-B ..
 typedef struct {
-   unsigned char Red;
-   unsigned char Green;
-   unsigned char Blue;
+    unsigned char Red;
+    unsigned char Green;
+    unsigned char Blue;
 }color; // .. named color.
 
 // Description : Creation of the definition of one LED (WS281x) ..
 typedef struct {
-   color colorPix;	      // color (Red, Green, Blue),
-   unsigned char status;   // status (ON : 1 / OFF : 0),
-   unsigned int pos;       // And a Position.
+    color colorPix;         // .. with a color,
+    unsigned char status;   // And a Status (ON : 1 / OFF : 0).
 }pixel; // .. named pixel.
 
-//-- GLOBAL VARIABLES	:------------------------------->
-// Declare an array of pixels defining the Matrix. Initialization in the 'C' file.
+//-- GLOBAL VARIABLES	:--------------------------------->
+// Declare an array of pixels defining the Strip. Initialization in the 'C' file.
 extern xdata pixel strip[MAX_LEDS];
 
-//-- GLOBAL MACROS ..	:------------------------------->
+//-- GLOBAL MACROS ..	:--------------------------------->
 // .. sending DATA '0' in Manchester  / Timing : 0 > 0.4[us] | 1 > 0.8[us] +- 150[ns] :
 // _nop_() is in <intrins.h> and waste a time machine, like a delay but scaling
 // on the CPU Clock.
-#define SEND0()	{\
-						SBIT_OUT_STRIP = 1;\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						SBIT_OUT_STRIP = 0;\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-					}
+#define SEND0() {\
+                    SBIT_OUT_STRIP = 1;\
+	                _nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					SBIT_OUT_STRIP = 0;\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+				}
 // .. sending DATA '1' in Manchester  / Timing : 0 > 0.85[us] | 1 > 0.45[us] +- 150[ns] :
 #define SEND1()	{\
-						SBIT_OUT_STRIP = 1;\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-						SBIT_OUT_STRIP = 0;\
-						_nop_();\
-						_nop_();\
-						_nop_();\
-					}
+                    SBIT_OUT_STRIP = 1;\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+					SBIT_OUT_STRIP = 0;\
+					_nop_();\
+					_nop_();\
+					_nop_();\
+				}
 
 // .. sending ONE bit of A specific color.
-#define	SEND_TO_LED(COLOR, MASK) { if((COLOR & MASK) != 0) {SEND1();} else{SEND0();} }
+#define SEND_TO_LED(COLOR, MASK) { if((COLOR & MASK) != 0) {SEND1();} else{SEND0();} }
 
 //-------------------------------------------------------------------------------------->
 // Function Prototypes :
-/* Description :  Filling color into the specific strip position.
- * Last_Update :  22.12.2020
- * Input		   :	addressStrip, 6bytes - address of the strip
- *						newColor, 3bytes - color to set
- *						position, 2bytes - position in the strip to set the color.
- * Output	 	:	Nothin'                                                                  */
-extern void pixel_SetColor(pixel* addressStrip, color newColor, \
-													 unsigned int position);
-/* Description :	Send the 24bits color of one pixel.
- * Last_Update :  22.12.2020
- * Input			:	red, 1byte.
- *						green, 1byte.
- *						blue, 1byte.
- * Output	 	:	Nothin'                                                                  */
+/* Description  :   Filling color into the specific strip position.
+ * Last_Update  :   22.12.2020
+ * Input		:   addressStrip, 6bytes - address of the strip
+ *					newColor, 3bytes - color to set
+ *					position, 2bytes - position in the strip to set the color.
+ * Output	 	:	Nothin'                                                            */
+extern void pixel_SetColor(pixel* addressStrip, color newColor, unsigned int position);
+/* Description  :   Send the 24bits color of one pixel.
+ * Last_Update  :   22.12.2020
+ * Input		:   red, 1byte.
+ *					green, 1byte.
+ *					blue, 1byte.
+ * Output	 	:	Nothin'                                                            */
 void pixel_Show(unsigned char red, unsigned char green, unsigned char blue);
 
-
-/* Description :	Clear color & status on all the strip + display it (or shut it).
- * Last_Update :  21.12.2020
- * Input		   :	addressStrip, 6bytes - address of the strip
- * Output	 	:	Nothin'                                                                  */
-extern void strip_Clear(pixel* addressStrip);
-/* Description :	Clear ONLY status on all the strip, to keep the color if needed.
- * Last_Update :  21.12.2020
- * Input		   :	addressStrip, 6bytes - address of the strip
- * Output	 	:	Nothin'                                                                  */
-extern void strip_StatusReset(pixel* addressStrip);
-/* Description :	Show the entire strip through output pin.
- * Last_Update :  21.12.2020
- * Input		   :	addressStrip, 6bytes - address of the strip
- * Output	 	:	Nothin'                                                                  */
+//-------------------------------------------------------------------------------------->
+/* Description  :   Show the entire strip through output pin.
+ * Last_Update  :   21.12.2020
+ * Input		:   addressStrip, 6bytes - address of the strip
+ * Output	 	:	Nothin'                                                            */
 extern void strip_Show(pixel* addressStrip);
+/* Description  :   Clear Color & Status on all the strip + display it (or shut it down).
+ * Last_Update  :   21.12.2020
+ * Input		:   addressStrip, 6bytes - address of the strip
+ * Output	 	:	Nothin'                                                            */
+extern void strip_Clear(pixel* addressStrip);
+/* Description  :   Clear ONLY status on all the strip, to keep the color if needed.
+ * Last_Update  :   21.12.2020
+ * Input	    :   addressStrip, 6bytes - address of the strip
+ * Output	 	:	Nothin'                                                            */
+extern void strip_StatusReset(pixel* addressStrip);
+/* Description  :   Reverse every LED status on all the strip, but keep the colors.
+ * Last_Update  :   24.12.2020
+ * Input	    :   addressStrip, 6bytes - address of the strip
+ * Output	 	:	Nothin'                                                            */
+extern void strip_StatusInverter(pixel* addressStrip);
 
-
+//-------------------------------------------------------------------------------------->
 // NeoPix of only a specific Color and Fading it.
-//void NeoPix_Fader(NeoPixel* _AddNeoPix, Color _NewCol, uint16 _ui16_Pos, uint16 _ui16_Speed);
+//void NeoPix_Fader(NeoPixel* _AddNeoPix, Color _NewCol, uint16 _ui16_Pos, \
+//                                                                  uint16 _ui16_Speed);
 // Board of only a specific Color on a specific position.
 extern void strip_LimitPos(pixel* addressStrip, color newColor, \
-														unsigned int _Begin, unsigned int _End);
-// Reversed every status of the strip.
-extern void strip_StatusInverter(pixel* _addMatrix);
+                                                unsigned int _Begin, unsigned int _End);
 
 
 #endif
